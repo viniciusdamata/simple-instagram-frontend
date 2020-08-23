da5d715d2f8ad1c522ea7215aec4cc3e854e3538
Vinicius Lima da Mata <viniciuslimadamata@gmail.com>
2020-08-23 18:33:51 -0400

    fix responsive

--- src/pages/Feed/styles.js
+++ src/pages/Feed/styles.js
@@ -3,8 +3,8 @@ import React from "react";
 import styled from "styled-components";
 
 export const PostList = styled.section`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
 
   @media (min-width: 600px) {
     display: flex;
@@ -13,12 +13,11 @@ export const PostList = styled.section`
   }
 `;
 
-
 export const LoadingPosts = styled.div`
-  display:flex;
-  align-items:center;
-  justify-content:center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   /* text-align:center; */
-  height:100vh;
-  font-size:20px;
-`
+  height: 100vh;
+  font-size: 20px;
+`;
